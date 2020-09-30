e3e9a6b2892c5de7ee33413b0dc3e6ff3206e6f8
Grzegorz Uriasz <gorbak25@gmail.com>
2020-09-30 20:38:01 +0200

    Compile and validate contracts

--- src/aesophia_rebar_plugin_prv.erl
+++ src/aesophia_rebar_plugin_prv.erl
@@ -42,7 +42,9 @@ do(State) ->
                 {_, undefined, _} ->
                     {error, "Please provide an output file"};
                 {_, _, false} ->
-                    compile(State, CompilerPath, CompilerVersion, ContractFilename, OutFilename)
+                    compile(State, CompilerPath, CompilerVersion, ContractFilename, OutFilename);
+                {_, _, true} ->
+                    verify(State, CompilerPath, CompilerVersion, ContractFilename, OutFilename)
             end;
         Err ->
             Err
@@ -60,7 +62,7 @@ compiler_path(Version, Verify) ->
     case { is_valid_version(Version)
          , compiler_supports_json_aci(Version)
          , compiler_supports_validation(Version)
-         , compiler_supports_fate(Version)} of
+         , compiler_supports_fate(Version) } of
         {false, _, _, _} ->
             {error, io_lib:format("~p is an unknown compiler version", [Version])};
         {_, false, _, _} ->
@@ -68,10 +70,10 @@ compiler_path(Version, Verify) ->
         {_, _, false, _} when Verify ->
             {error, io_lib:format("Compiler ~p is too old to support bytecode validation", [Version])};
         {_, _, _, false} ->
-            %% TODO: add AEVM support
+            %% TODO: add AEVM support - for now just FATE
             {error, io_lib:format("Compiler ~p is too old to support FATE", [Version])};
         _ ->
-              {ok, filename:join([code:priv_dir(aesophia_cli), "bin", Version, "aesophia_cli"])}
+            {ok, filename:join([code:priv_dir(aesophia_cli), "bin", Version, "aesophia_cli"])}
     end.
 
 is_valid_version(Version) ->
@@ -88,7 +90,7 @@ compiler_supports_validation([$v, $3 | _]) -> false;
 compiler_supports_validation([$v, $2 | _]) -> false;
 compiler_supports_validation([$v, $1 | _]) -> false.
 
-compiler_supports_fate([$v, $4]) -> true;
+compiler_supports_fate([$v, $4 | _]) -> true;
 compiler_supports_fate(_) -> false.
 
 compile(State, CompilerPath, Version, InFilename, OutFilename) ->
@@ -103,4 +105,71 @@ compile(State, CompilerPath, Version, InFilename, OutFilename) ->
     end.
 
 compile_(State, CompilerPath, Version, InFilename, OutFilename) ->
+    rebar_api:info("Compiling and generating the ACI of ~p using aesophia ~p saving results in ~p", [InFilename, Version, OutFilename]),
+    CompilationOutput = os:cmd(format_compilation_command(CompilerPath, Version, InFilename)),
+    ACIOutput = os:cmd(format_aci_command(CompilerPath, Version, InFilename)),
+    Bytecode = postprocess_compilation(CompilationOutput, Version),
+    ACI = postprocess_aci(ACIOutput, Version),
+    file:write_file(OutFilename, jsx:encode(#{<<"bytecode">> => Bytecode, <<"aci">> => ACI})),
+    rebar_api:info("Compilation successfull", []),
+    {ok, State}.
 
+verify(State, CompilerPath, Version, InFilename, OutFilename) ->
+    case {filelib:is_regular(InFilename), file:read_file(OutFilename)} of
+        {false, _} ->
+            {error, io_lib:format("Unable to read the sophia contract: ~p", [InFilename])};
+        {_, {error, _}} ->
+            {error, io_lib:format("Unable to read compilation result: ~p", [OutFilename])};
+        {_, {ok, Output}} ->
+             case try
+                  #{ <<"bytecode">> := B
+                   , <<"aci">> := JText} = jsx:decode(Output, [{return_maps,false},{labels,binary}]),
+                  aeaci_aci:from_string(JText, #{backend => fate}),
+                  <<"cb_", _/binary>> = B,
+                  {ok, B}
+                catch _:_ ->
+                    {error, "Invalid compilation results. Do not trust the contract!"}
+                end of
+                    {ok, Bytecode} ->
+                        verify_(State, CompilerPath, Version, InFilename, Bytecode);
+                    {error, _} = Err ->
+                        Err
+            end
+    end.
+
+verify_(State, CompilerPath, Version, InFilename, Bytecode) ->
+    try
+        <<"Validation successful", _/binary>> =
+            os:cmd(format_validation_command(CompilerPath, Version, InFilename, Bytecode)),
+        rebar_api:info("Validation successfull", []),
+        {ok, State}
+    catch
+        _:_ ->
+            {error, "Invalid compilation results. Do not trust the contract!"}
+    end.
+
+format_compilation_command(CompilerPath, [$v, $4 | _], InFilename) ->
+    io_lib:format("~p ~p", [CompilerPath, InFilename]).
+
+postprocess_compilation(CompilerOutput, [$v, $4 | _]) ->
+    try
+        <<"Bytecode:\n", Bytecode/binary>> = CompilerOutput,
+        string:strip(binary_to_list(Bytecode))
+    catch E:R ->
+        rebar_api:abort("Compilation failed ~p ~p", [E, R])
+    end.
+
+format_aci_command(CompilerPath, _Version, InFilename) ->
+    io_lib:format("~p --create_json_aci ~p", [CompilerPath, InFilename]).
+
+postprocess_aci(CompilerOutput, [$v, $4 | _]) ->
+    try
+        JText = list_to_binary(CompilerOutput),
+        aeaci_aci:from_string(JText, #{backend => fate}),
+        JText
+    catch E:R:S ->
+        rebar_api:format("ACI generation failed ~p ~p ~p", [E, R, S])
+    end.
+
+format_validation_command(CompilerPath, _Version, InFilename, Bytecode) ->
+    io_lib:format("~p ~p --validate \"~p\"", [CompilerPath, InFilename, Bytecode]).
